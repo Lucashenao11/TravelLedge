cd1e68cb0a3ada79127e1423fffeb443dbd267d6
Lucashenao11 <lucas.henao11@gmail.com>
2025-02-28 15:51:35 -0500

    PR1

---
+++ src/infoCurso.ts
@@ -0,0 +1,52 @@
+let infoCurso = {
+    matematicas: [
+        {
+            id: 1,
+            titulo: "Matematicas 1",
+            descripcion: "Curso de matematicas 1",
+            duracion: "2 meses",
+            costo: 100
+        },
+        {
+            id: 2,
+            titulo: "Matematicas 2",
+            descripcion: "Curso de matematicas 2",
+            duracion: "2 meses",
+            costo: 100
+        }
+    ],
+    Fisica: [
+        {
+            id: 1,
+            titulo: "Fisica 1",
+            descripcion: "Curso de fisica 1",
+            duracion: "2 meses",
+            costo: 100
+        },
+        {
+            id: 2,
+            titulo: "Fisica 2",
+            descripcion: "Curso de fisica 2",
+            duracion: "2 meses",
+            costo: 100
+        }
+    ],
+    Quimica: [
+        {
+            id: 1,
+            titulo: "Quimica 1",
+            descripcion: "Curso de quimica 1",
+            duracion: "2 meses",
+            costo: 100
+        },
+        {
+            id: 2,
+            titulo: "Quimica 2",
+            descripcion: "Curso de quimica 2",
+            duracion: "2 meses",
+            costo: 100
+        }
+    ]
+}
+
+export{ infoCurso }
